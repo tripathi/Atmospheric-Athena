af9d6d78f179462a9d4804e5023460475c978d6c
Anjali Tripathi <atripathi@cfa.harvard.edu>
2012-01-10 15:42:45 -0800

    Modified ionrad.c to be athena v4 compliant. Need to ensure arguments are passed to it correctly

--- src/ionradiation/ionrad.c
+++ src/ionradiation/ionrad.c
@@ -23,44 +23,50 @@
 #ifdef ION_RADIATION
 static int dim=0;
 
-void ion_radtransfer_init_domain(Grid *pGrid, Domain *pDomain) {
+void ion_radtransfer_init_domain(DomainS *pD) {
 
-  /* Calcualte the dimensionality and error check */
+  /*Set grid - new to Athena v4*/
+  GridS *pG = (pD->Grid);
+
+  /* Calculate the dimensionality and error check */
   dim = 0;
-  if(pGrid->Nx1 > 1) dim++;
-  if(pGrid->Nx2 > 1) dim++;
-  if(pGrid->Nx3 > 1) dim++;
+  if(pG->N[0] > 1) dim++;
+  if(pG->N[1] > 1) dim++;
+  if(pG->N[2] > 1) dim++;
 
   switch(dim) {
   case 1: break;
   case 2: break;
   case 3:
-    ion_radtransfer_init_domain_3d(pGrid, pDomain);
+    ion_radtransfer_init_domain_3d(pG, pD);
     return;
   }
 
-  ath_error("[ion_radtransfer_init_domain]: Unsupported dim. Nx1=%d, Nx2=%d, Nx3=%d\n",
-	    pGrid->Nx1,pGrid->Nx2,pGrid->Nx3);
+  ath_error("[ion_radtransfer_init_domain]: Unsupported dim. N[0]=%d, N[1]=%d, N[2]=%d\n",
+	    pG->N[0],pG->N[1],pG->N[2]);
 }
 
-VGFun_t ion_radtransfer_init(Grid *pGrid, Domain *pDomain, int ires){
+VGFun_t ion_radtransfer_init(DomainS *pD, int ires){
+
+  /*Set grid - new to Athena v4*/
+  GridS *pG = (pD->Grid);
 
   /* Calcualte the dimensionality and error check */
   dim = 0;
-  if(pGrid->Nx1 > 1) dim++;
-  if(pGrid->Nx2 > 1) dim++;
-  if(pGrid->Nx3 > 1) dim++;
+  if(pG->N[0] > 1) dim++;
+  if(pG->N[1] > 1) dim++;
+  if(pG->N[2] > 1) dim++;
 
   switch(dim){
   case 1: break;
   case 2: break;
   case 3:
-    ion_radtransfer_init_3d(pGrid, pDomain, ires);
+    ion_radtransfer_init_3d(pG, pD, ires);
     return ion_radtransfer_3d;
   }
 
-  ath_error("[ion_radtransfer_init]: Unsupported dim. Nx1=%d, Nx2=%d, Nx3=%d\n",
-	    pGrid->Nx1,pGrid->Nx2,pGrid->Nx3);
+  ath_error("[ion_radtransfer_init]: Unsupported dim. N[0]=%d, N[1]=%d, N[2]=%d\n",
+	    pG->N[0],pG->N[1],pG->N[2]);
 
   /* This is never executed, but lack of a return statement generates
      a warning on some compilers. */
