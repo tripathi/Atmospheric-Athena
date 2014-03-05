11185a1da075dd8e16714eda1a38db0491886b75
Anjali Tripathi <atripathi@cfa.harvard.edu>
2014-03-05 05:02:04 -0500

    Adding needed string header include to problem file

--- src/prob/ioniz_sphere.c
+++ src/prob/ioniz_sphere.c
@@ -8,6 +8,7 @@
  *============================================================================*/
 
 #include <math.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "defs.h"
